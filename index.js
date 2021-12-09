f34f633c1fdbd525ebb1be62167e8e2d81460dc9
RAM SANTOSH GEESALA <71482536+santosh2697@users.noreply.github.com>
2021-12-09 12:13:23 +0530

    nearby code implemented

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-
-
-
-
-
-//create avatar
-
-
-
-// for(let i=0;i<10;i++){
-// let w= Math.floor(Math.random()*1000);
-// let h= Math.floor(Math.random()*700);
-// var red = Math.floor(Math.random() * 255);
-// var green = Math.floor(Math.random() * 255);
-// var blue = Math.floor(Math.random() * 255);
-// // ctx.fillStyle = '#8ED6FF';
-// ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + " )";  
-// ctx.fillRect(w,h,20,20);
-// }
-
-
-//function to generate random colors
-// function generateRandomColor() {
-// var letters = '0123456789ABCDEF';
-// var color = '#';
-// for (var i = 0; i < 6; i++) {
-// color += letters[Math.floor(Math.random() * 16)];
-// }
-// return color;
-// }
-// for(let i=0;i<10;i++){
-// let w= Math.floor(Math.random()*1000);
-// let h= Math.floor(Math.random()*700);
-// ctx.fillStyle=generateRandomColor();
-// ctx.fillRect(w,h,20,20);
-// }
-
+//Global varibales
+var obj = {};
+var id;
+let x=0;
+let y=0;
+let w= 500;
+let h= 500;
 
 //Function to move the avatar
 function move(e){
+
+    
     if(e.keyCode==39){
         x+=5;
     }
@@ -50,82 +22,53 @@ function move(e){
     if(e.keyCode==40){
         y+=5;
     }
-    c.width=c.width;
-    ctx.drawImage(img2,x,y)
-    ctx.drawImage(img1, w, h); 
+    loadCanvas(id);
+
+    //Checking  avatars in proximity
+    nearby();
+}
+
+
+//Function to check nearby avatars
+function nearby() {
+
+    console.log(obj);
+    var distance = Math.floor(Math.sqrt(Math.pow((obj["B"][0] - obj["A"][0]),2) + Math.pow((obj["B"][1] - obj["A"][1]),2) ))
+
+    if (distance < 250) {
+        console.log("Avatar nearby turn on mic and video");
+    }
 }
 
 
 //keydown event calls move fn whenever arrow keys are pressed.
 document.onkeydown=move;
 
-function createAvatar(){
-  var cardbody=  `<div class="card mt-5" style="width: 28rem;  margin:auto;">
-  <div class="card-body">
-    <h5 class="card-title" style="text-align:center;">Gather Town</h5>
-    <p class="card-text" style="text-align:center;">Select your avatar!!</p>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-            <img src="1.png" style="width:50px; height:50px;" onclick="loadCanvas(1)"/>
-            </div>
-            <div class="col">
-            <img src="2.png" style="width:50px; height:50px;" onclick="loadCanvas(2)"/>
-            </div>
-            <div class="col">
-            <img src="3.png" style="width:50px; height:50px;" onclick="loadCanvas(3)"/>
-            </div>
-            <div class="col">
-            <img src="4.png" style="width:50px; height:50px;" onclick="loadCanvas(4)"/>
-            </div>
-        </div>
-        <br/>
-
-        <div class="row">
-            <div class="col">
-            <img src="5.png" style="width:50px; height:50px;" onclick="loadCanvas(5)"/>
-            </div>
-            <div class="col">
-            <img src="6.png" style="width:50px; height:50px;" onclick="loadCanvas(6)"/>
-            </div>
-            <div class="col">
-            <img src="7.png" style="width:50px; height:50px;" onclick="loadCanvas(7)"/>
-            </div>
-            <div class="col">
-            <img src="8.png" style="width:50px; height:50px;" onclick="loadCanvas(8)"/>
-            </div>
-        </div>
-    </div>
-  </div>
-</div>`
-document.getElementById("root").innerHTML=cardbody;
-}
-
 //function to display the canvas
-function loadCanvas(id){
+function loadCanvas(pic){
 
-    console.log(id);
-    var player1=id+'.png';
+    id = pic;
+
     var canvas = `<canvas id="myCanvas" width="1000" height="700" style="border:1px solid #000000; background-image: url('office.png'); background-size: contain;"></canvas>`
     document.getElementById("root").innerHTML=canvas;
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
+
     //required when avatar images are added
     var img1 = new Image();
     var img2 = new Image(); 
+    
+    var player1=pic+'.png';
     img1.src = player1;
-    console.log(player1)
-    // img2.src = '8.png';
-    let x=0;
-    let y=0;
+    img2.src = '8.png';
 
-    let w= Math.floor(Math.random()*1000);
-    let h= Math.floor(Math.random()*700);
     //Creating new images
     img1.onload = () => { 
         ctx.drawImage(img1, w, h);
+        obj["A"] = [w,h];
     };
-    // img2.onload = () => { 
-    //     ctx.drawImage(img2, x, y);
-    // };
+    img2.onload = () => { 
+        ctx.drawImage(img2, x, y);        
+        obj["B"] = [x,y];
+    };
 }
